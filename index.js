dc257f80f71071c79e5aa20499d669f720b2af5e
Tchoupinax <corentinfiloche@hotmail.fr>
2017-09-11 14:19:25 +0200

    Refonte

--- index.js
+++ index.js
@@ -1,96 +1,87 @@
 const colors = require('colors');
 wantStderr = false;
+const textColor = "\x1b[30m";
+const reset = "\x1b[0m";
+const data = {
+    greenBg: "\x1b[42m",
+    greenFG: "\x1b[32m",
+    yellowBg: "\x1b[43m",
+    yellowFg: "\x1b[33m",
+    redBg: "\x1b[41m",
+    redFg: "\x1b[31m",
+    blueBg: "\x1b[44m",
+    blueFg: "\x1b[36m",
+};
+
 /*
  * ===============================================================
  * ==== O V E R R I D E ==========================================
  * ====================== C O N S O L E . I N F O ================
  * ===============================================================
  */
-const dataI = {
-    greenBg: "\x1b[42m",
-    greenFG: "\x1b[32m",
-    textColor: "\x1b[30m",
-    reset: "\x1b[0m",
-};
 console.old_info = console.info;
-console.info = function() {
-    console.old_info(dataI.greenBg + dataI.textColor, "[INFO]",
-        dataI.reset, dataI.greenFG, ...arguments, dataW.reset);
+console.info = function () {
+    console.old_info(data.greenBg + textColor, "[INFO]",
+        reset, data.greenFG, ...arguments, reset);
 };
 module.exports = console.info;
+
 /*
  * ===============================================================
  * ==== O V E R R I D E ==========================================
  * ====================== C O N S O L E . W A R N ================
  * ===============================================================
  */
-const dataW = {
-    yellowBg: "\x1b[43m",
-    yellowFg: "\x1b[33m",
-    textColor: "\x1b[30m",
-    reset: "\x1b[0m",
-};
 console.old_warn = console.warn;
-console.warn = function() {
-    console.old_warn(dataW.yellowBg + dataW.textColor, "[WARNING]",
-        dataW.reset, dataW.yellowFg, ...arguments, dataW.reset);
+console.warn = function () {
+    console.old_warn(data.yellowBg + textColor, "[WARNING]",
+        reset, data.yellowFg, ...arguments, reset);
 };
 module.exports = console.warn;
+
 /*
  * ===============================================================
  * ==== O V E R R I D E ==========================================
  * ====================== C O N S O L E . E R R O R ==============
  * ===============================================================
  */
-const dataE = {
-    redBg: "\x1b[41m",
-    redFg: "\x1b[31m",
-    textColor: "\x1b[30m",
-    reset: "\x1b[0m",
-};
 if (wantStderr) { console.old_error = console.error; } else { console.old_error = console.other; }
-console.error = function() {
-    console.log(dataE.redBg + dataE.textColor, "[ERROR]", dataE.reset, dataE.redFg, ...arguments, dataE.reset, dataW.reset, dataW.reset);
+console.error = function () {
+    console.log(data.redBg + textColor, "[ERROR]",
+        reset, data.redFg, ...arguments, reset);
 };
 module.exports = console.error;
+
 /*
  * ===============================================================
  * ==== O V E R R I D E ==========================================
  * ====================== C O N S O L E . D E B U G ==============
  * ===============================================================
  */
-const dataD = {
-    blueBg: "\x1b[44m",
-    blueFg: "\x1b[36m",
-    textColor: "\x1b[30m",
-    reset: "\x1b[0m",
-};
 console.old_debug = console.log;
-console.debug = function() {
-    console.old_debug(dataD.blueBg + dataD.textColor, "[DEBUG]", dataD.reset, dataD.blueFg, ...arguments, dataW.reset, dataW.reset);
+console.debug = function () {
+    console.old_debug(data.blueBg + textColor, "[DEBUG]",
+        reset, data.blueFg, ...arguments, reset);
 };
 module.exports = console.debug;
 
-
-
-console.old_log = console.log;
-console.log = function() {
-    console.old_log(...arguments, dataW.reset);
-};
 /*
  * ===============================================================
  * ==== O V E R R I D E ==========================================
  * ====================== C O N S O L E . D E B U G ==============
  * ===============================================================
  */
-//module.exports = console.log;
-/*
- *   Print empty line
- */
-exports.ln = function(msg) {
-    console.log("\n");
+console.old_log = console.log;
+console.log = function () {
+    console.old_log(...arguments, reset);
 };
+module.exports = console.log
 
+/*
+ * ===============================================================
+ * ==== P R I V A T E ============================================
+ * ===============================================================
+ */
 function readInLogFile(msg) {
     const path = (require("shelljs").pwd()).stdout;
     var fs = require('fs');
